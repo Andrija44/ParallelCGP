ba821c52fc924290d9d319a834ccb4d38ae15a39
Andrija Macek <macekandrija@gmail.com>
2025-04-09 05:05:50 +0200

    Added a simple choice selector

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstdlib> 
 #include <ctime>
+#include "Problem.h"
 #include "BoolProblem.h"
 #include "FuncProblem.h"
 
@@ -11,9 +12,23 @@ using namespace std;
 using namespace parallel_cgp;
 
 int main() {
-    FuncProblem fp;
+    int choice;
+    cout << "Choose which problem to run" << endl << endl;
+    cout << "1 - Boolean problem" << endl;
+    cout << "2 - Function problem" << endl;
+    cout << endl << "Enter your choice: ";
+    cin >> choice;
 
-    fp.problemSimulator();
+    Problem* problem = nullptr;
+
+    if (choice == 1)
+        problem = new BoolProblem;
+    else if (choice == 2)
+        problem = new FuncProblem;
+    else
+        cout << "Invalid option" << endl;
+
+    problem->problemSimulator();
 
     return 0;
 }
