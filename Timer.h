43e528b095b8f2dbec6c1565597b68b1936434d9
Andrija Macek <andrija.macek@fer.unizg.hr>
2025-04-11 19:57:05 +0200

    Add custom timer class

---
+++ Timer.h
@@ -0,0 +1,56 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <omp.h>
+#include <chrono>
+#include <map>
+#include <string>
+#include <functional>
+#include <iostream>
+
+#if defined(_OPENMP)
+#define timerFunc() omp_get_wtime()
+#define timerDiff(startTime, endTime) (endTime - startTime)
+#define TIME_UNIT double
+#else
+#define timerFunc() std::chrono::steady_clock::now()
+#define timerDiff(startTime, endTime) (std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count() / 1000000.0)
+#define TIME_UNIT std::chrono::steady_clock::time_point
+#endif
+
+namespace parallel_cgp {
+
+	class Timer
+	{
+	private:
+		inline static std::map<std::string, double> mapa;
+
+		std::string funcName;
+		TIME_UNIT start;
+		double end;
+	public:
+		Timer(std::string funcName) : start(timerFunc()), end(0), funcName(funcName) { 
+			#pragma omp critical
+			{
+				if (!parallel_cgp::Timer::mapa.count(funcName))
+					parallel_cgp::Timer::mapa[funcName] = 0;
+			}
+		}
+
+		void endTimer() {
+			end = timerDiff(start, timerFunc());
+
+			#pragma omp critical
+			parallel_cgp::Timer::mapa[funcName] += end;
+			
+		}
+	
+		static void printTimes() {
+			for (const auto& [key, value] : parallel_cgp::Timer::mapa)
+				std::cout << '[' << key << "] = " << value << "; " << std::endl;
+		}
+	};
+}
+
+#endif
+
